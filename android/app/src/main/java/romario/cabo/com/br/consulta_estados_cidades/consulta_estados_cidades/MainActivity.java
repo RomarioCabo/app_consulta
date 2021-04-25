430b3c6c3a111e93677c9331743c81b5b4f279b5
Romário Cabó <romariocabo2012@gmail.com>
2021-04-25 13:38:19 -0300

    first commit

---
+++ android/app/src/main/java/romario/cabo/com/br/consulta_estados_cidades/consulta_estados_cidades/MainActivity.java
@@ -0,0 +1,6 @@
+package romario.cabo.com.br.consulta_estados_cidades.consulta_estados_cidades;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
